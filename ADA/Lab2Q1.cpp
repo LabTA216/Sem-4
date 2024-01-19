3548258f27789293898c44118a5f1a1c4ce6ae75
TA123 <lwork0921.com>
2024-01-19 14:32:00 +0530

    ADA L2

--- ADA/Lab2Q1.cpp
+++ ADA/Lab2Q1.cpp
@@ -1,12 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Min-max  
+
+
+void minMax(vector<int> arr, int n, int min, int max){
+    for (int i = 1; i < n ; i++)
+    {
+        if( arr[i] < min){
+            min = arr[i];
+        }
+        else if ( arr[i] > max){
+            max = arr[i];
+        }
+    }
+    
+    cout << "\nMin : " << min;
+    cout << "\nMin : " << max << endl;
+
+}
+
 
 
 int main(int argc, char const *argv[])
 {
 
-    
+    vector<int> arr = {1,2,3,4,5,6};
+    int min = arr[0], max = arr[0];
+
+    minMax(arr,6,min,max);
 
     return 0;
 }
